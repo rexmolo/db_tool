ba28d0e81bb0d58e7b914c0b66a9117c57af06fb
mh1988 <397091486@qq.com>
2015-10-26 14:57:09 +0800

    test

--- config/db_dictionary.php
+++ config/db_dictionary.php
@@ -7,30 +7,3 @@
  */
 
 
-$db['usersinfo'] = array(
-	'host'      => '122.4.79.213',
-	'db'        => 'zhiliao_usersinfo',
-	'userName'  => 'xzhiliao',
-	'dbPass'    => '^YHNmju7'
-);
-
-$db['comsinfo'] = array(
-	'host'      => '122.4.79.213',
-	'db'        => 'zhiliao_comsinfo',
-	'userName'  => 'xzhiliao',
-	'dbPass'    => '^YHNmju7'
-);
-
-$db['chart'] = array(
-	'host'      => '122.4.79.213',
-	'db'        => 'zhiliao_chat',
-	'userName'  => 'xzhiliao',
-	'dbPass'    => '^YHNmju7'
-);
-
-$db['hr_ucenter'] = array(
-	'host'      => '122.4.79.213',
-	'db'        => 'hr_ucenter',
-	'userName'  => 'xzhiliao',
-	'dbPass'    => '^YHNmju7'
-);
